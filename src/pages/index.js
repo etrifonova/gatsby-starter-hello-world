cf06d05fe8ccc12c95745083a17911ba22f5f880
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2024-08-05 19:35:52 +0300

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
-import * as React from "react"
+import * as React from "react";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      Hello world!
+    </div>
+  );
 }
