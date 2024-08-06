e3f56e79ae37ddd523c7a977a4e773fd5fb3db28
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2024-08-06 20:47:57 +0300

    feat: styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import Layout from '../components/Layout'
+import * as aboutStyles from '../examples/about.module.css'
 
 const about = () => {
   return (
-    <div>
-      <Navbar />
-        This is the About page
-    </div>
+    <Layout>
+      <div className={aboutStyles.page}>
+        <h2>This is the About page</h2>
+        <p className={aboutStyles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eaque maxime tempora modi vel blanditiis fugit? Sunt praesentium distinctio quisquam exercitationem consequuntur repellat tempora natus. Repellat nostrum rerum numquam provident!
+        Corrupti, beatae suscipit soluta nemo itaque ex id necessitatibus sapiente aut nam magni qui excepturi, ratione iusto architecto pariatur quia autem vitae quisquam. Sequi, officia odio corporis veniam odit aliquid.
+        Laboriosam odio atque amet porro quasi totam aspernatur expedita voluptates, error, quia molestiae blanditiis cupiditate optio. Rem, distinctio molestiae eaque esse asperiores tempora soluta natus, consequuntur facilis quidem cumque non.</p>
+      </div>
+    </Layout>
   )
 }
 
