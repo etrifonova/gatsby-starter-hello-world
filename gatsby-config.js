14859da2976cdea13b5f62ded73d3d4be030a4f6
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2024-08-15 17:44:48 +0300

    add stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,17 @@
 module.exports = {
   // pathPrefix: "/https://github.com/etrifonova/gatsby-starter-hello-world",
   pathPrefix: "/gatsby-starter-hello-world",
+  siteMetadata: {
+    title: 'Simply Recipes',
+    description: 'Nice and clean recipes site',
+    author: '@etrifonova',
+    person: {name: 'elena', age: 34},
+    simpleData:['item 1', 'item 2'],
+    complexData: [
+      {name: 'elena', age: 34},
+      {name: 'denis', age: 36},
+    ],
+  },
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
